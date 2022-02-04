ed3fc3c851be302adfa0ade9f9b3da3b5ea97efa
Abdelghafour <ghafourragg@gmail.com>
2022-02-04 01:47:00 +0100

    updated alot

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,6 @@
 const lis = document.querySelectorAll(".nav-ul li.glob");
 const subLis = document.querySelectorAll(".pop");
+const burgerMenu = document.querySelector(".burger");
 
 lis.forEach((li) => {
   li.addEventListener("click", () => {
@@ -12,4 +13,16 @@ lis.forEach((li) => {
     );
     subLi.classList.toggle("fade-in");
   });
+});
+
+let openSvg =
+  "http://127.0.0.1:5500/blogr-landingpage/images/icon-hamburger.svg";
+let closeSvg = "http://127.0.0.1:5500/blogr-landingpage/images/icon-close.svg";
+burgerMenu.addEventListener("click", () => {
+  let inImg = burgerMenu.querySelector("img");
+  if (inImg.src == openSvg) {
+    inImg.src = closeSvg;
+  } else {
+    inImg.src = openSvg;
+  }
 });
