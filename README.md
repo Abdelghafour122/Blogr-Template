fe93542da2fc96f31281c762a18da6812fe83efb
Abdelghafour <ghafourragg@gmail.com>
2022-02-24 15:18:01 +0100

    finished the template

--- README.md
+++
@@ -1,3 +0,0 @@
-# Frontend-Mentor-Challenge-Blogr-Template  
-I tried coding the Blogr Template from Frontend Mentor, this is my first time using SCSS instead of raw CSS in a web template which i found really usefull.  
-![desktop-preview](https://user-images.githubusercontent.com/63031509/154758596-3d880e7b-fcf4-4b60-8cf0-9033ee27469f.jpg)
--- app/js/script.js
+++ app/js/script.js
@@ -1,19 +1,7 @@
 const lis = document.querySelectorAll(".nav-ul li.glob");
 const subLis = document.querySelectorAll(".pop");
 const burgerMenu = document.querySelector(".burger");
-
-lis.forEach((li) => {
-  li.addEventListener("click", () => {
-    const subLi = li.querySelector(".pop");
-    subLis.forEach(
-      (sLi) =>
-        sLi !== subLi &&
-        sLi.classList.contains("fade-in") &&
-        sLi.classList.remove("fade-in")
-    );
-    subLi.classList.toggle("fade-in");
-  });
-});
+const links = document.querySelector(".links");
 
 let openSvg =
   "http://127.0.0.1:5500/blogr-landingpage/images/icon-hamburger.svg";
@@ -23,7 +11,9 @@ burgerMenu.addEventListener("click", () => {
   let inImg = burgerMenu.querySelector("img");
   if (inImg.src == openSvg) {
     inImg.src = closeSvg;
+    links.style.display = "flex";
   } else {
     inImg.src = openSvg;
+    links.style.display = "none";
   }
 });
