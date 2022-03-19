b9ba337e6af8267f8788f244c0d95df3fb577128
Abdelghafour <ghafourragg@gmail.com>
2022-03-19 16:06:36 +0100

    added main, fixed menu button

--- app/js/script.js
+++ app/js/script.js
@@ -1,15 +1,12 @@
-const lis = document.querySelectorAll(".nav-ul li.glob");
-const subLis = document.querySelectorAll(".pop");
 const burgerMenu = document.querySelector(".burger");
 const links = document.querySelector(".links");
 
-let openSvg =
-  "http://127.0.0.1:5500/blogr-landingpage/images/icon-hamburger.svg";
-let closeSvg = "http://127.0.0.1:5500/blogr-landingpage/images/icon-close.svg";
+let openSvg = "./images/icon-hamburger.svg";
+let closeSvg = "./images/icon-close.svg";
 
 burgerMenu.addEventListener("click", () => {
   let inImg = burgerMenu.querySelector("img");
-  if (inImg.src == openSvg) {
+  if (inImg.src.includes(openSvg.slice(1))) {
     inImg.src = closeSvg;
     links.style.display = "flex";
   } else {
